aa04937821afdaf0888da48bc64b3387eb2dcdcf
Walmyr Filho <wlsf82@gmail.com>
2017-09-02 17:01:59 +0200

    remove unused logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import PageContent from "./components/PageContent";
 import PageFooter from "./components/PageFooter";
 import PageHeader from "./components/PageHeader";
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {

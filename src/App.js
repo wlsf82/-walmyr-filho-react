89b6473f412b94c1fbf60a6007563d9f00345612
Walmyr Filho <wlsf82@gmail.com>
2017-09-04 00:39:12 +0200

    create and include mobile css style sheet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PageFooter from "./components/PageFooter";
 import PageHeader from "./components/PageHeader";
 import React, { Component } from "react";
 import "./App.css";
+import "./AppMobile.css";
 
 class App extends Component {
     render() {

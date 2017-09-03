999417b94b00dff55d1e41c5b886bb7006576b22
Walmyr Filho <wlsf82@gmail.com>
2017-09-03 19:47:11 +0200

    update app with background on blog component and hover effect on links

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,13 +1,17 @@
 import React, { PureComponent } from "react";
-import tatLogo from "./assets/tat.png";
+import tatBackground from "./assets/tatBackground.jpg";
+
+const backgroundStyle = {
+    backgroundImage: "url(" + tatBackground + ")"
+};
 
 class Blog extends PureComponent {
     render() {
         return (
-            <div className="blog">
+            <div className="blog" style={ backgroundStyle}>
                 <h2>Blog</h2>
                 <a href="https://talkingabouttesting.com" target="_blank" rel="noopener noreferrer">
-                    <img src={tatLogo} alt="Talking About Testing"/>
+                    <h3>Talking About Testing</h3>
                 </a>
                 <p>Criado para compartilhar conhecimento sobre meus aprendizados ao longo da carreira, com conteúdos para reflexão, mas também com muito conteúdo "mão na massa"</p>
             </div>

9c960bbf8607007fa441dfc7520bd7915c865a1f
Walmyr Filho <wlsf82@gmail.com>
2017-09-04 00:38:52 +0200

    update components using button to pass label argument

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,7 +1,12 @@
 import React, { PureComponent } from "react";
+import Button from "../Button";
 import bookImage from "./assets/bookImg.png"
 
 class Book extends PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="book">
@@ -10,6 +15,7 @@ class Book extends PureComponent {
                     <a href="https://www.casadocodigo.com.br/products/livro-protractor" target="_blank" rel="noopener noreferrer">Protractor - Lições sobre testes end-to-end automatizados</a>
                 </h3>
                 <p>Neste livro você irá encontrar uma coleção de práticas para implementar testes end-to-end automatizados ao processo de desenvolvimento de aplicações web, utilizando o framework Protractor.</p>
+                <Button>Compre o livro</Button>
                 <a href="https://www.casadocodigo.com.br/products/livro-protractor" target="_blank" rel="noopener noreferrer">
                     <img src={bookImage} />
                 </a>
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,5 +1,6 @@
 import Button from "../Button";
 import React, { PureComponent } from "react";
+const serviceButtonLabel = "Saiba mais sobre o serviço";
 
 class Services extends PureComponent {
     render() {
@@ -9,17 +10,17 @@ class Services extends PureComponent {
                 <div>
                     <h3>Coaching</h3>
                     <p>Ajudo profissionais a se desenvolverem para conseguirem melhores oportunidades no mercado, contribuir em comunidades, morar no exterior, e muito mais</p>
-                    <Button />
+                    <Button>{ serviceButtonLabel }</Button>
                 </div>
                 <div>
                     <h3>Consultoria</h3>
                     <p>Ajudo empresas e profissionais a implementarem práticas de programação extrema, para que entreguem software de qualidade aos seus clientes</p>
-                    <Button />
+                    <Button>{ serviceButtonLabel }</Button>
                 </div>
                 <div>
                     <h3>Desenvolvimento</h3>
                     <p>Desenvolvo testes automatizados de UI utilizando ferramentas modernas e boas práticas de mercado. Terceirize testes de UI com quem já tem isso "no sangue"</p>
-                    <Button />
+                    <Button>{ serviceButtonLabel }</Button>
                 </div>
             </div>
         );

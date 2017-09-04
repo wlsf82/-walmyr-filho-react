39cae4ef9d0c62d94b91d13f11e40ae2599445ad
Walmyr Filho <wlsf82@gmail.com>
2017-09-04 22:51:25 +0200

    refactor services component

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,27 +1,33 @@
 import Button from "../Button";
 import React, { PureComponent } from "react";
 const serviceButtonLabel = "Saiba mais sobre o serviço";
+const services = [
+    {
+        h3: "Coaching",
+        p: "Ajudo profissionais a se desenvolverem para conseguirem melhores oportunidades no mercado, contribuir em comunidades, morar no exterior, e muito mais",
+    },
+    {
+        h3: "Consultoria",
+        p: "Ajudo empresas e profissionais a implementarem práticas de programação extrema, para que entreguem software de qualidade aos seus clientes",
+    },
+    {
+        h3: "Desenvolvimento",
+        p: "Desenvolvo testes automatizados de UI utilizando ferramentas modernas e boas práticas de mercado. Terceirize testes de UI com quem já tem isso \"no sangue\"",
+    },
+];
 
 class Services extends PureComponent {
     render() {
         return (
             <div className="services">
                 <h2>Serviços</h2>
-                <div>
-                    <h3>Coaching</h3>
-                    <p>Ajudo profissionais a se desenvolverem para conseguirem melhores oportunidades no mercado, contribuir em comunidades, morar no exterior, e muito mais</p>
-                    <Button>{ serviceButtonLabel }</Button>
-                </div>
-                <div>
-                    <h3>Consultoria</h3>
-                    <p>Ajudo empresas e profissionais a implementarem práticas de programação extrema, para que entreguem software de qualidade aos seus clientes</p>
-                    <Button>{ serviceButtonLabel }</Button>
-                </div>
-                <div>
-                    <h3>Desenvolvimento</h3>
-                    <p>Desenvolvo testes automatizados de UI utilizando ferramentas modernas e boas práticas de mercado. Terceirize testes de UI com quem já tem isso "no sangue"</p>
-                    <Button>{ serviceButtonLabel }</Button>
-                </div>
+                { services.map( item =>
+                    <div>
+                        <h3>{item.h3}</h3>
+                        <p>{item.p}</p>
+                        <Button>{ serviceButtonLabel }</Button>
+                    </div>
+                )}
             </div>
         );
     }

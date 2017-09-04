c761397a3e3d7b561de44baaa85be7d7d4b1b4e5
Walmyr Filho <wlsf82@gmail.com>
2017-09-04 22:57:17 +0200

    refactor courses component

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -1,28 +1,36 @@
 import React, { PureComponent } from "react";
 
+const courses = [
+    {
+        href: "https://www.youtube.com/playlist?list=PL-eblSNRj0QEvVfKp0Xzagao9SqaSyW5k",
+        title: "Aprendendo Protractor",
+        p: "Curso gratúito no canal Talking About Testing, no Youtube, sobre testes automatizados com Protractor (do básico ao avançado)",
+    },
+    {
+        href: "https://github.com/wlsf82/protractor-and-webrtc",
+        title: "Protractor & WebRTC",
+        p: "Codelab em minha conta no GitHub sobre como criar testes automatizados para aplicações WebRTC com o framework Protractor",
+    },
+    {
+        href: "http://code-squad.com/curso/Curso-Protractor-Automacao-de-testes-end-to-end-para-aplicacoes-Angular-JS/avulso",
+        title: "Protractor & AngularJS",
+        p: "Curso na plataforma do Code Squad sobre automação de testes para aplicações AngularJS com o framework Protractor",
+    },
+];
+
 class Courses extends PureComponent {
     render() {
         return (
             <div className="courses">
                 <h2>Cursos</h2>
+                { courses.map(item =>
                 <div>
                     <h3>
-                        <a href="https://www.youtube.com/playlist?list=PL-eblSNRj0QEvVfKp0Xzagao9SqaSyW5k" target="_blank" rel="noopener noreferrer">Aprendendo Protractor</a>
+                        <a href={item.href} target="_blank" rel="noopener noreferrer">{item.title}</a>
                     </h3>
-                    <p>Curso gratúito no canal Talking About Testing, no Youtube, sobre testes automatizados com Protractor (do básico ao avançado)</p>
+                    <p>{item.p}</p>
                 </div>
-                <div>
-                    <h3>
-                        <a href="https://github.com/wlsf82/protractor-and-webrtc" target="_blank" rel="noopener noreferrer">Protractor & WebRTC</a>
-                    </h3>
-                    <p>Codelab em minha conta no GitHub sobre como criar testes automatizados para aplicações WebRTC com o framework Protractor</p>
-                </div>
-                <div>
-                    <h3>
-                        <a href="http://code-squad.com/curso/Curso-Protractor-Automacao-de-testes-end-to-end-para-aplicacoes-Angular-JS/avulso" target="_blank" rel="noopener noreferrer">Protractor & AngularJS</a>
-                    </h3>
-                    <p>Curso na plataforma do Code Squad sobre automação de testes para aplicações AngularJS com o framework Protractor</p>
-                </div>
+                )}
             </div>
         );
     }

9c3bb5fa64c7dd0359ffec8f399ad92ea384d6ad
Walmyr Filho <wlsf82@gmail.com>
2017-09-04 23:07:11 +0200

    refactor speaches component

--- src/components/Speaches/index.js
+++ src/components/Speaches/index.js
@@ -1,43 +1,70 @@
 import React, { PureComponent } from "react";
 
+const leftSideSpeaches = [
+    {
+        href: "http://talkingabouttesting.com/tag/n-ways-to-test/",
+        title: "N ways to test 2015",
+    },
+    {
+        href: "http://guts-rs.blogspot.com.br/2015/08/saiba-o-que-rolou-no-evento-de.html",
+        title: "GUTS-RS Agosto de 2015",
+    },
+    {
+        href: "http://www.thedevelopersconference.com.br/tdc/2015/florianopolis/trilha-testes",
+        title: "TDC Floripa 2015",
+    },
+    {
+        href: "http://pt.slideshare.net/walmyrlimasilvafilho/agile-testing-coach-agile-trends-floripa",
+        title: "Agile Trends Floripa 2015",
+    },
+    {
+        href: "http://pt.slideshare.net/walmyrlimasilvafilho/protractor-visual-review-2",
+        title: "TDC Porto Alegre 2015",
+    },
+];
+
+const rightSideSpeaches = [
+    {
+        href: "http://www.slideshare.net/walmyrlimasilvafilho/primeiros-passos-com-protractor-gutssc",
+        title: "GUTS-SC Janeiro 2016",
+    },
+    {
+        href: "http://www.slideshare.net/walmyrlimasilvafilho/protractor-style-guide-agile-testers-conference-2016",
+        title: "Conferência Agile Testers 2016",
+    },
+    {
+        href: "https://goo.gl/11VEhl",
+        title: "TDC Floripa 2016",
+    },
+    {
+        href: "https://www.slideshare.net/walmyrlimasilvafilho/agile-testing-coach-agile-lean-ireland-2017",
+        title: "Agile Lean Ireland 2017",
+    },
+    {
+        href: "https://www.slideshare.net/walmyrlimasilvafilho/estrategias-para-o-uso-de-integracao-e-entrega-continua-com-gocd",
+        title: "Conferência Agile Testers 2017",
+    },
+];
+
 class Speaches extends PureComponent {
     render() {
         return (
             <div className="speaches">
                 <h2>Palestras e eventos</h2>
                 <div className="left">
+                { leftSideSpeaches.map(item =>
                     <h3>
-                        <a href="http://talkingabouttesting.com/tag/n-ways-to-test/" target="_blank" rel="noopener noreferrer">N ways to test 2015</a>
+                        <a href={item.href} target="_blank" rel="noopener noreferrer">{item.title}</a>
                     </h3>
-                    <h3>
-                        <a href="http://guts-rs.blogspot.com.br/2015/08/saiba-o-que-rolou-no-evento-de.html" target="_blank" rel="noopener noreferrer">GUTS-RS Agosto de 2015</a>
-                    </h3>
-                    <h3>
-                        <a href="http://www.thedevelopersconference.com.br/tdc/2015/florianopolis/trilha-testes" target="_blank" rel="noopener noreferrer">TDC Floripa 2015</a>
-                    </h3>
-                    <h3>
-                        <a href="http://pt.slideshare.net/walmyrlimasilvafilho/agile-testing-coach-agile-trends-floripa" target="_blank" rel="noopener noreferrer">Agile Trends Floripa 2015</a>
-                    </h3>
-                    <h3>
-                        <a href="http://pt.slideshare.net/walmyrlimasilvafilho/protractor-visual-review-2" target="_blank" rel="noopener noreferrer">TDC Porto Alegre 2015</a>
-                    </h3>
+                )}
+
                 </div>
                 <div className="right">
+                { rightSideSpeaches.map(item =>
                     <h3>
-                        <a href="http://www.slideshare.net/walmyrlimasilvafilho/primeiros-passos-com-protractor-gutssc" target="_blank" rel="noopener noreferrer">GUTS-SC Janeiro 2016</a>
+                        <a href={item.href} target="_blank" rel="noopener noreferrer">{item.title}</a>
                     </h3>
-                    <h3>
-                        <a href="http://www.slideshare.net/walmyrlimasilvafilho/protractor-style-guide-agile-testers-conference-2016" target="_blank" rel="noopener noreferrer">Conferência Agile Testers 2016</a>
-                    </h3>
-                    <h3>
-                        <a href="https://goo.gl/11VEhl" target="_blank" rel="noopener noreferrer">TDC Floripa 2016</a>
-                    </h3>
-                    <h3>
-                        <a href="https://www.slideshare.net/walmyrlimasilvafilho/agile-testing-coach-agile-lean-ireland-2017" target="_blank" rel="noopener noreferrer">Agile Lean Ireland 2017</a>
-                    </h3>
-                    <h3>
-                        <a href="https://www.slideshare.net/walmyrlimasilvafilho/estrategias-para-o-uso-de-integracao-e-entrega-continua-com-gocd" target="_blank" rel="noopener noreferrer">Conferência Agile Testers 2017</a>
-                    </h3>
+                )}
                 </div>
             </div>
         );

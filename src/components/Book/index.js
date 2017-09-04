2ac7b1b281e993a6752a08965dc22a5a5253c188
Walmyr Filho <wlsf82@gmail.com>
2017-09-04 22:42:45 +0200

    reuse book url on book component

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from "react";
 import Button from "../Button";
 import bookImage from "./assets/bookImg.png"
+const bookUrl = "https://www.casadocodigo.com.br/products/livro-protractor";
 
 class Book extends PureComponent {
     constructor(props) {
@@ -12,11 +13,11 @@ class Book extends PureComponent {
             <div className="book">
                 <h2>Livro</h2>
                 <h3>
-                    <a href="https://www.casadocodigo.com.br/products/livro-protractor" target="_blank" rel="noopener noreferrer">Protractor - Lições sobre testes end-to-end automatizados</a>
+                    <a href={bookUrl} target="_blank" rel="noopener noreferrer">Protractor - Lições sobre testes end-to-end automatizados</a>
                 </h3>
                 <p>Neste livro você irá encontrar uma coleção de práticas para implementar testes end-to-end automatizados ao processo de desenvolvimento de aplicações web, utilizando o framework Protractor.</p>
                 <Button>Compre o livro</Button>
-                <a href="https://www.casadocodigo.com.br/products/livro-protractor" target="_blank" rel="noopener noreferrer">
+                <a href={bookUrl} target="_blank" rel="noopener noreferrer">
                     <img src={bookImage} alt="Livro de Protractor"/>
                 </a>
             </div>

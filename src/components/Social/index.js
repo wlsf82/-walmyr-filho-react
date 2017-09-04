c7c06fbfc0c54d1962bac3adba87818fe9948bfe
Walmyr Filho <wlsf82@gmail.com>
2017-09-04 22:21:01 +0200

    refactor social component

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,37 +1,59 @@
 import React, { PureComponent } from "react";
-import facebookIcon from "./assets/facebook.png";
-import githubIcon from "./assets/github.png";
-import googleplusIcon from "./assets/googleplus.png";
-import linkedinIcon from "./assets/linkedin.png";
-import twiterIcon from "./assets/twiter.png";
-import wordpressIcon from "./assets/wordpress.png";
-import youtubeIcon from "./assets/youtube.png";
+import facebookImg from "./assets/facebook.png";
+import githubImg from "./assets/github.png";
+import googleplusImg from "./assets/googleplus.png";
+import linkedinImg from "./assets/linkedin.png";
+import twiterImg from "./assets/twiter.png";
+import wordpressImg from "./assets/wordpress.png";
+import youtubeImg from "./assets/youtube.png";
+
+const socialData = [
+    {
+        alt: "Facebook",
+        img: facebookImg,
+        href: "https://www.facebook.com/walmyr.limasilvafilho",
+    },
+    {
+        alt: "GitHub",
+        img: githubImg,
+        href: "https://github.com/wlsf82",
+    },
+    {
+        alt: "Google Plus",
+        img: googleplusImg,
+        href: "https://plus.google.com/114754442377170940541",
+    },
+    {
+        alt: "LinkedIn",
+        img: linkedinImg,
+        href: "https://br.linkedin.com/in/walmyr-lima-e-silva-filho-147a9110a",
+    },
+    {
+        alt: "Twiter",
+        img: twiterImg,
+        href: "https://twitter.com/walmyrlimaesilv",
+    },
+    {
+        alt: "Wordpress",
+        img: wordpressImg,
+        href: "https://talkingabouttesting.com/",
+    },
+    {
+        alt: "YouTube",
+        img: youtubeImg,
+        href: "https://www.youtube.com/talkingabouttesting",
+    },
+];
 
 class Social extends PureComponent {
     render() {
         return (
             <div className="social">
-                <a href="https://www.facebook.com/walmyr.limasilvafilho" target="_blank" rel="noopener noreferrer">
-                    <img src={facebookIcon} alt="Facebook"/>
+            { socialData.map( item =>
+                <a href={item.href} target="_blank" rel="noopener noreferrer">
+                    <img src={item.img} alt={item.alt}/>
                 </a>
-                <a href="https://github.com/wlsf82" target="_blank" rel="noopener noreferrer">
-                    <img src={githubIcon} alt="GitHub"/>
-                </a>
-                <a href="https://plus.google.com/114754442377170940541" target="_blank" rel="noopener noreferrer">
-                    <img src={googleplusIcon} alt="Google Plus"/>
-                </a>
-                <a href="https://br.linkedin.com/in/walmyr-lima-e-silva-filho-147a9110a" target="_blank" rel="noopener noreferrer">
-                    <img src={linkedinIcon} alt="LinkedIn"/>
-                </a>
-                <a href="https://twitter.com/walmyrlimaesilv" target="_blank" rel="noopener noreferrer">
-                    <img src={twiterIcon} alt="Twiter"/>
-                </a>
-                <a href="https://talkingabouttesting.com/" target="_blank" rel="noopener noreferrer">
-                    <img src={wordpressIcon} alt="Wordpress"/>
-                </a>
-                <a href="https://www.youtube.com/talkingabouttesting" target="_blank" rel="noopener noreferrer">
-                    <img src={youtubeIcon} alt="Youtube"/>
-                </a>
+            )}
             </div>
         );
     }

8cfc2e3ae42facd5f941dce87a133e32086db2cb
Walmyr Filho <wlsf82@gmail.com>
2017-09-05 17:37:11 +0200

    change button component to be an anchor and to accept other props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,12 +2,17 @@ import React, { PureComponent } from "react";
 
 class Button extends PureComponent {
     render() {
-        const { children } = this.props;
+        const { href, target = "_self", children } = this.props;
 
         return (
-            <button>
-                { children }
-            </button>
+            <a
+                className="button"
+                href={href}
+                target={target}
+                rel="noopener noreferrer"
+            >
+                    { children }
+            </a>
         );
     }
 }

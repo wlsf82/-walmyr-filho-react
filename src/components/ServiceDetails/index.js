7f14e3114009c81b4ad347b176d55b69666d43e7
Walmyr Filho <wlsf82@gmail.com>
2017-09-06 22:47:37 +0200

    create service details component (wip)

---
+++ src/components/ServiceDetails/index.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+
+class ServiceDetails extends PureComponent {
+    render() {
+        return (
+            <div className="main-content">
+                <h2>Foo</h2>
+                <p>foobarbaz</p>
+            </div>
+        );
+    }
+}
+
+export default ServiceDetails;

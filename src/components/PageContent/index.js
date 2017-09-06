a85638573fca552b6030bebd9a011d38c66bea46
Walmyr Filho <wlsf82@gmail.com>
2017-09-06 22:47:07 +0200

    update page content component to render content given a condition (wip)

--- src/components/PageContent/index.js
+++ src/components/PageContent/index.js
@@ -2,13 +2,25 @@ import Blog from "../Blog";
 import Book from "../Book";
 import Courses from "../Courses";
 import Introduction from "../Introduction";
+import ServiceDetails from "../ServiceDetails";
 import Services from "../Services";
 import Speaches from "../Speaches";
 import React, { PureComponent } from "react";
 
 class PageContent extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            page: "home",
+        }
+    }
+
     render() {
+        const { page } = this.state;
+
         return (
+            page === "home" ?
             <div className="main-content">
                 <Introduction />
                 <Services />
@@ -16,6 +28,9 @@ class PageContent extends PureComponent {
                 <Courses />
                 <Book />
                 <Speaches />
+            </div> :
+            <div className="service-details">
+                <ServiceDetails />
             </div>
         );
     }

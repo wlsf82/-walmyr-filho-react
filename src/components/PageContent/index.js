2754249f27e3499af28e04cfb01e7e51df2f1e8d
Walmyr Filho <wlsf82@gmail.com>
2017-11-12 23:35:03 +0100

    remove not used code from project

--- src/components/PageContent/index.js
+++ src/components/PageContent/index.js
@@ -2,34 +2,21 @@ import Blog from "../Blog";
 import Book from "../Book";
 import Courses from "../Courses";
 import Introduction from "../Introduction";
-import ServiceDetails from "../ServiceDetails";
 import Services from "../Services";
 import Speaches from "../Speaches";
 import React, { PureComponent } from "react";
 
 class PageContent extends PureComponent {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            page: "home",
-        }
-    }
-
     render() {
-        const { page } = this.state;
-
         return (
-            page !== "home" ?
-                <ServiceDetails>{page}</ServiceDetails> :
-                <div className="main-content">
-                    <Introduction />
-                    <Courses />
-                    <Blog />
-                    <Services />
-                    <Book />
-                    <Speaches />
-                </div>
+            <div className="main-content">
+                <Introduction />
+                <Courses />
+                <Blog />
+                <Services />
+                <Book />
+                <Speaches />
+            </div>
         );
     }
 }

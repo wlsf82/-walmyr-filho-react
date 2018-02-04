1f95610895c7c9c262c6617e1a471a7e9347c435
wlsf82 <wlsf82@gmail.com>
2018-02-04 21:59:11 +0100

    fix link on header

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -5,7 +5,7 @@ class PageHeader extends PureComponent {
         return (
             <div className="App-header">
                 <h1>
-                    <a href="">Walmyr Lima e Silva Filho</a>
+                    <a href="#">Walmyr Lima e Silva Filho</a>
                 </h1>
             </div>
         );

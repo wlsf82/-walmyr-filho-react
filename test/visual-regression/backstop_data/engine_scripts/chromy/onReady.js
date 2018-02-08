49c46f1d163d91f494cd225419508f5dbf4d009e
wlsf82 <wlsf82@gmail.com>
2018-02-08 19:27:13 +0100

    use es6 string interpolation instead of es5

--- test/visual-regression/backstop_data/engine_scripts/chromy/onReady.js
+++ test/visual-regression/backstop_data/engine_scripts/chromy/onReady.js
@@ -1,4 +1,4 @@
 module.exports = function (chromy, scenario) {
-    console.log("SCENARIO > " + scenario.label); // eslint-disable-line no-console
+    console.log(`SCENARIO >  ${scenario.label}`); // eslint-disable-line no-console
     require("./clickAndHoverHelper")(chromy, scenario);
 };

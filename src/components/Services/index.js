be60cb9bc01a06a6424fbfaa48d15b8a08b8b658
wlsf82 <wlsf82@gmail.com>
2018-02-19 00:54:20 +0100

    update some services with link on their headers

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -7,11 +7,13 @@ const services = [
         id: uuid(),
         h3: "Coaching",
         p: "Ajudo profissionais a se desenvolverem para melhor se posicionarem no mercado, contribuir em comunidades, trabalhar no exterior, e muito mais",
+        href: "https://talkingabouttesting.com/mentoria-coaching/",
     },
     {
         id: uuid(),
         h3: "Consultoria",
         p: "Ajudo empresas e profissionais a implementarem práticas de programação extrema, lean e DevOps, para que entreguem software de qualidade aos seus clientes com agilidade",
+        href: "https://talkingabouttesting.com/servicos/consultoria/",
     },
     {
         id: uuid(),
@@ -21,12 +23,20 @@ const services = [
 ];
 
 class Services extends PureComponent {
+    showServiceHeaderAsLinkOrNot(service) {
+        return service.href ?
+            <h3>
+                <a href={service.href} target="_blank">{service.h3}</a>
+            </h3> :
+            <h3>{service.h3}</h3>
+    }
+
     render() {
         return (
             <div className="services">
                 { services.map(item =>
                     <div key={item.id}>
-                        <h3>{item.h3}</h3>
+                        { this.showServiceHeaderAsLinkOrNot(item) }
                         <p>{item.p}</p>
                         <Button
                             href={ `mailto: wlsf82@gmail.com?Subject=Informações sobre serviço de ${item.h3}` }

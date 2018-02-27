a56548278084c7fa45d3f5a7abe3e3c6bc2a05a3
wlsf82 <wlsf82@gmail.com>
2018-02-27 15:36:27 +0100

    simplify files removing console logs and comments

--- test/visual-regression/backstop_data/engine_scripts/onBefore.js
+++ test/visual-regression/backstop_data/engine_scripts/onBefore.js
@@ -1,4 +1,1 @@
-module.exports = function (engine, scenario, viewport) {
-    // This script runs before your app loads. Edit here to log-in, load cookies or set other states required for your test.
-    console.log(`onBefore.js has run for ${viewport.label}.`); // eslint-disable-line no-console
-};
+module.exports = function (engine, scenario, viewport) {};

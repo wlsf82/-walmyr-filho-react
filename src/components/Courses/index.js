bc3cd716141da9de60aa84bc35140d47f77dd728
wlsf82 <wlsf82@gmail.com>
2018-05-10 00:59:44 +0200

    simplify courses' descriptions

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -6,19 +6,19 @@ const courses = [
         id: uuid(),
         href: "http://talkingabouttesting.coursify.me/courses/introducao-aos-testes-automatizados",
         title: "Testes automatizados",
-        p: "Curso gratúito teórico no Coursify.me sobre automação de testes em diversas camadas de aplicações",
+        p: "Curso teórico sobre automação de testes em diversas camadas de aplicações",
     },
     {
         id: uuid(),
         href: "http://talkingabouttesting.coursify.me/courses/arquitetura-de-testes-com-protractor",
         title: "Protractor",
-        p: "Curso prático no Coursify.me sobre arquitetura de testes end-to-end com Protractor",
+        p: "Curso prático sobre arquitetura de testes end-to-end com o framework Protractor",
     },
     {
         id: uuid(),
         href: "http://talkingabouttesting.coursify.me/courses/testes-de-regressao-visual-com-backstopjs",
         title: "BackstopJS",
-        p: "Curso prático no Coursify.me sobre testes de regressão visual com a ferramenta BackstopJS",
+        p: "Curso prático sobre testes de regressão visual com a ferramenta BackstopJS",
     },
 ];
 

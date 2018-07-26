4a2288f2582150c3f9ea9b2b12ca1f399785c4c7
Walmyr <wlsf82@gmail.com>
2018-07-26 23:31:10 +0200

    simplify services component with href value for development service

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -18,25 +18,20 @@ const services = [
         id: uuid(),
         h3: "Desenvolvimento",
         p: "Desenvolvo testes end-to-end automatizados utilizando ferramentas modernas e boas práticas de mercado. Terceirize testes de UI em seus projetos com quem já tem isso \"no sangue\"",
+        href: "https://talkingabouttesting.com/servicos/desenvolvimento/",
     },
 ];
 
 class Services extends PureComponent {
-    showServiceHeaderAsLinkOrNot(service) {
-        return service.href ?
-            <h3>
-                <a href={service.href} target="_blank">{service.h3}</a>
-            </h3> :
-            <h3>{service.h3}</h3>
-    }
-
     render() {
         return (
             <div className="services blocks">
-                { services.map(item =>
-                    <div key={item.id}>
-                        { this.showServiceHeaderAsLinkOrNot(item) }
-                        <p>{item.p}</p>
+                { services.map(service =>
+                    <div key={service.id}>
+                        <h3>
+                            <a href={service.href} target="_blank">{service.h3}</a>
+                        </h3>
+                        <p>{service.p}</p>
                     </div>
                 )}
             </div>

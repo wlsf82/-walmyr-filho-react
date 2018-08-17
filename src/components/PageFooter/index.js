4685fb58ea8c8efd0bd2b0f8a8a7ceeba87223b3
Walmyr <wlsf82@gmail.com>
2018-08-17 18:03:01 +0200

    remove href from footer link
    This was using a mailto, which is not very good in terms of UX.

--- src/components/PageFooter/index.js
+++ src/components/PageFooter/index.js
@@ -7,7 +7,7 @@ import Social from "../Social";
 const PageFooter = () =>
   <div className="App-footer">
     <h4>Entre em contato</h4>
-    <a href="mailto: wlsf82@gmail.com?Subject=Olá">wlsf82@gmail.com</a>
+    <a>wlsf82@gmail.com</a>
     <Social />
   </div>
 

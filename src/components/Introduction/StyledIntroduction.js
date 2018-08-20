05899908ad232cb5f8dd0e2a6d47440c9e0f7adb
Walmyr <wlsf82@gmail.com>
2018-08-20 20:41:49 +0200

    add missing mobile style for styled introduction component

--- src/components/Introduction/StyledIntroduction.js
+++ src/components/Introduction/StyledIntroduction.js
@@ -4,6 +4,7 @@ const StyledIntroduction = styled.div`
   padding-bottom: 80px;
   @media only screen and (max-device-width: 650px) {
     padding: 12px;
+    border-bottom: 5px dotted #f2f2f2;
   }
 `;
 

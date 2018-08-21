403f7abc9d778fc2cc029e9b5b073a3b6eeb58ae
Walmyr <wlsf82@gmail.com>
2018-08-21 20:12:52 +0200

    style blog component using styled-components lib

---
+++ src/components/Blog/StyledBlog.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+import tatBackground from "./assets/tatBackground.jpg";
+
+const StyledBlog = styled.div`
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-image: url(${tatBackground});
+  background-size: cover;
+  color: #fff;
+  opacity: .9;
+  padding-top: 30px;
+  @media only screen and (max-device-width: 650px) {
+    background-size: 0;
+    border-bottom: 5px dotted #f2f2f2;
+  }
+`;
+
+const StyledBlogH2 = styled.h2`
+  @media only screen and (max-device-width: 650px) {
+    color: #7a7c84;
+  }
+`;
+
+const StyledBlogParagraph = styled.p`
+  margin-bottom: 0;
+  margin-left: 15%;
+  margin-right: 15%;
+  padding-bottom: 40px;
+  padding-left: 200px;
+  padding-right: 200px;
+  @media only screen and (max-device-width: 650px) {
+    color: #7a7c84;
+		margin-left: 0;
+		margin-right: 0%;
+		padding: 12px;
+  }
+  @media only screen and (max-device-width: 768px) {
+    padding-left: 0;
+		padding-right: 0;
+  }
+`;
+
+const StyledBlogAnchor = styled.a`
+  color: #efa700;
+  &:hover {
+    color: #f2f2f2;
+  }
+  @media only screen and (max-device-width: 650px) {
+    color: #2c313f;
+    &:hover {
+      color: #efa700;
+    }
+  }
+`;
+
+export { StyledBlogH2, StyledBlogParagraph, StyledBlogAnchor };
+
+export default StyledBlog;
--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,21 +1,14 @@
 import React from "react";
 
-import "./index.css";
-import "./index-mobile.css";
-
-import tatBackground from "./assets/tatBackground.jpg";
-
-const backgroundStyle = {
-    backgroundImage: `url(${tatBackground})`
-};
+import StyledBuyBookButton, { StyledBlogH2, StyledBlogParagraph, StyledBlogAnchor } from "./StyledBlog";
 
 const Blog = () =>
-  <div className="blog" style={backgroundStyle}>
-    <h2>Blog</h2>
-    <a href="https://talkingabouttesting.com" target="_blank" rel="noopener noreferrer">
+  <StyledBuyBookButton>
+    <StyledBlogH2>Blog</StyledBlogH2>
+    <StyledBlogAnchor href="https://talkingabouttesting.com" target="_blank" rel="noopener noreferrer">
       <h3>Talking About Testing</h3>
-    </a>
-    <p>Criado para compartilhar conhecimento sobre meus aprendizados ao longo da carreira, com conteúdos para reflexão, mas também com muito conteúdo "mão na massa"</p>
-  </div>
+    </StyledBlogAnchor>
+    <StyledBlogParagraph>Criado para compartilhar conhecimento sobre meus aprendizados ao longo da carreira, com conteúdos para reflexão, mas também com muito conteúdo "mão na massa"</StyledBlogParagraph>
+  </StyledBuyBookButton>
 
 export default Blog;

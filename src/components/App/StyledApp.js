0b675a0f19eca519e4fa7c4ea365ee35b451492c
Walmyr <wlsf82@gmail.com>
2018-08-22 00:24:21 +0200

    use injectGlobal from lib instead of having a main css file
    The mentioned lib is styled-components.

--- src/components/App/StyledApp.js
+++ src/components/App/StyledApp.js
@@ -1,4 +1,34 @@
-import styled from "styled-components";
+import styled, { injectGlobal } from "styled-components";
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+
+  *,
+  *:before,
+  *:after {
+    padding: 0;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+  }
+
+  a {
+    color: #2c313f;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: #efa700;
+  }
+
+  h1, h2, h3 {
+    text-transform: uppercase;
+  }
+`;
 
 const StyledApp = styled.div`
   font-family: "Lato", sans-serif;

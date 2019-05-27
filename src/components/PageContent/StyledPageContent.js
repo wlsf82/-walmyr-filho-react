9587788015e25d790f328dba9dc409bdfb7402e5
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2019-05-27 12:13:36 +0200

    Simplify PageContent styles
    - Change grey color to darker grey to improve contrast / acessibility;
    - Remove unecessary @media styles;

--- src/components/PageContent/StyledPageContent.js
+++ src/components/PageContent/StyledPageContent.js
@@ -1,12 +1,9 @@
 import styled from "styled-components";
 
 const StyledPageContent = styled.div`
-  color: #7a7c84;
+  color: #707070;
   line-height: 150%;
-  padding-top: 160px;
-  @media (max-width: 650px) {
-    padding-top:100px;
-  }
+  padding-top: 100px;
 `;
 
 export default StyledPageContent;

a21c69fbe03b850735afba8fab71d72a9e366a1c
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2019-05-31 00:28:30 +0200

    WIP: Book section
    - Add new Book cover image;
    - Add .wrapper to hold content;
    - Use single column layout for mobile resolution;
    - Use two column layout for desktop resolution;
    - Add comment header to improve code legibility;
    - Define all <img> to 100% width to make it easy to deal with responsive images;

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-import StyledBook, { StyledBlogParagraph, StyledBookImage } from "./StyledBook";
+import StyledBook, {
+  StyledBookContainer,
+  StyledBookH2,
+  StyledBookH3,
+  StyledBookH4,
+  StyledBlogParagraph,
+  StyledBookImage,
+  StyleBookDescription,
+  StyleBookPreview
+} from "./StyledBook";
 
 import BuyBookButton from "../BuyBookButton";
-import bookImage from "./assets/bookImg.png"
+import bookImage from "./assets/bookcover.png"
 
 const bookUrl = "https://www.casadocodigo.com.br/products/livro-protractor";
 const relValue = "noopener noreferrer";
@@ -11,15 +20,22 @@ const targetValue = "_blank";
 
 const Book = () =>
   <StyledBook>
-    <h2>Livro</h2>
-    <h3>
-      <a href={bookUrl} target={targetValue} rel={relValue}>Protractor - Lições sobre testes end-to-end automatizados</a>
-    </h3>
-    <StyledBlogParagraph>Neste livro você encontrará uma coleção de boas práticas para implementar testes end-to-end automatizados ao processo de desenvolvimento de aplicações web, utilizando o framework Protractor.</StyledBlogParagraph>
-    <BuyBookButton />
-    <a href={bookUrl} target={targetValue} rel={relValue}>
-      <StyledBookImage src={bookImage} alt="Livro de Protractor"/>
-    </a>
+    <div className="wrapper">
+      <StyledBookContainer>
+        <StyleBookDescription>
+          <StyledBookH2>Livro</StyledBookH2>
+          <StyledBookH3>Protractor</StyledBookH3>
+          <StyledBookH4>Lições sobre testes end-to-end automatizados</StyledBookH4>
+          <StyledBlogParagraph>Neste livro você encontrará uma coleção de boas práticas para implementar testes end-to-end automatizados ao processo de desenvolvimento de aplicações web, utilizando o framework Protractor.</StyledBlogParagraph>
+          <BuyBookButton />
+        </StyleBookDescription>
+        <StyleBookPreview>
+          <a href={bookUrl} target={targetValue} rel={relValue}>
+            <StyledBookImage src={bookImage} alt="Livro: Protractor - Lições sobre testes end-to-end automatizados"/>
+          </a>
+        </StyleBookPreview>
+      </StyledBookContainer>
+    </div>
   </StyledBook>
 
 export default Book;

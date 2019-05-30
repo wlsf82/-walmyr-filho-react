61c403ec51f36192b604e565716ba1d81160087b
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2019-05-30 19:57:20 +0200

    Introduction improvements
    - Remove unecessary media query;
    - Use 24px base for padings;

--- src/components/Introduction/StyledIntroduction.js
+++ src/components/Introduction/StyledIntroduction.js
@@ -3,12 +3,8 @@ import styled from "styled-components";
 const StyledIntroduction = styled.div`
   display: flex;
   align-items: center;
-  min-height: 30vh;
-  padding-bottom: 20px;
-
-  @media (max-width: 650px) {
-    border-bottom: 5px dotted #f2f2f2;
-  }
+  min-height: 24vh;
+  padding-bottom: 24px;
 `;
 
 export default StyledIntroduction;

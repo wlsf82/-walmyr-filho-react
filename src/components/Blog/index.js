6be1f43b5cd103655a2aa2899992e7267368d0ab
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2019-05-30 23:07:00 +0200

    Rework Blog session
    - For some reason, blog component was using "StyledBuyBookButton" to import styles. I'ts now renamed to StyledBlog;
    - Add .wrapper to hold blog content;
    - Create <StyledBlogWrapper/> to centrilize blog content;
    - Move <StyledBlogAnchor> to end of session and style it as a button;
    - Set background in a pseudo element to allow opacity only on it and not in the content;
    - Use background-blend-mode to set sepia color to background image;
    - Add Comment headers to better code legibility;
    -

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 
-import StyledBuyBookButton, { StyledBlogH2, StyledBlogParagraph, StyledBlogAnchor } from "./StyledBlog";
+import StyledBlog, {
+  StyledBlogWrapper,
+  StyledBlogH2,
+  StyledBlogH3,
+  StyledBlogParagraph,
+  StyledBlogAnchor
+} from "./StyledBlog";
 
 const Blog = () =>
-  <StyledBuyBookButton>
-    <StyledBlogH2>Blog</StyledBlogH2>
-    <StyledBlogAnchor href="https://talkingabouttesting.com" target="_blank" rel="noopener noreferrer">
-      <h3>Talking About Testing</h3>
-    </StyledBlogAnchor>
-    <StyledBlogParagraph>Criado para compartilhar conhecimento sobre meus aprendizados ao longo da carreira, com conteúdos para reflexão, mas também com muito conteúdo "mão na massa"</StyledBlogParagraph>
-  </StyledBuyBookButton>
+  <StyledBlog>
+    <div className="wrapper">
+      <StyledBlogWrapper>
+        <StyledBlogH2>Blog</StyledBlogH2>
+        <StyledBlogH3>Talking About Testing</StyledBlogH3>
+        <StyledBlogParagraph>Criado para compartilhar conhecimento sobre meus aprendizados ao longo da carreira, com conteúdos para reflexão, mas também com muito conteúdo "mão na massa"</StyledBlogParagraph>
+        <StyledBlogAnchor
+            href="https://talkingabouttesting.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Leia mais</StyledBlogAnchor>
+      </StyledBlogWrapper>
+    </div>
+  </StyledBlog>
 
 export default Blog;

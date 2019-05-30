10ab7a17d2073fd38408b0e4a7bd84db16cdf728
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2019-05-31 01:01:35 +0200

    WIP: Playing around introduction section

--- src/components/Introduction/StyledIntroduction.js
+++ src/components/Introduction/StyledIntroduction.js
@@ -6,8 +6,20 @@ import styled from "styled-components";
 const StyledIntroduction = styled.div`
   display: flex;
   align-items: center;
-  min-height: 24vh;
+  min-height: 40vh;
   padding-bottom: 24px;
+  font-weight: 100;
+  font-size: 120%;
+  line-height: 120%;
+  background-color: #ffffff;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1327' height='1327' viewBox='0 0 100 100'%3E%3Cg stroke='%23CCC' stroke-width='26.1' stroke-opacity='0.12'%3E%3Crect fill='%23F5F5F5' x='-60' y='-60' width='110' height='240'/%3E%3C/g%3E%3C/svg%3E");
+
+@media (min-width: 960px) {
+  min-height: 60vh;
+  font-size: 180%;
+  text-align: left;
+}
+
 `;
 
 export default StyledIntroduction;

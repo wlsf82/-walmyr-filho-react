005803a2543b04fb26f4bd26b9409dd9d0c53293
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2019-06-01 17:10:41 +0200

    Move PageHeader to mobile-first approach
    - Remove externally linked styles;
    - Invert @media-query loginc from max-width to min-width (Mobile-first approach);
    - Font adjustments for Introduction;

--- src/components/Introduction/StyledIntroduction.js
+++ src/components/Introduction/StyledIntroduction.js
@@ -15,8 +15,8 @@ const StyledIntroduction = styled.div`
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1327' height='1327' viewBox='0 0 100 100'%3E%3Cg stroke='%23CCC' stroke-width='26.1' stroke-opacity='0.12'%3E%3Crect fill='%23F5F5F5' x='-60' y='-60' width='110' height='240'/%3E%3C/g%3E%3C/svg%3E");
 
 @media (min-width: 960px) {
-  min-height: 60vh;
-  font-size: 180%;
+  min-height: 50vh;
+  font-size: 160%;
   text-align: left;
 }
 `;

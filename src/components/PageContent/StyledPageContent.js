dc135404197af679b2e0f036f05b328a68a83aff
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2019-06-01 17:10:41 +0200

    Move PageHeader to mobile-first approach
    - Remove externally linked styles;
    - Invert @media-query loginc from max-width to min-width (Mobile-first approach);
    - Font adjustments for Introduction;

--- src/components/PageContent/StyledPageContent.js
+++ src/components/PageContent/StyledPageContent.js
@@ -5,7 +5,11 @@ import styled from "styled-components";
 ////////////////////////////////////////////////////////////////////////////////
 const StyledPageContent = styled.div`
   line-height: 150%;
-  padding-top: 100px;
+  padding-top: 75px; /* Header height for mobile */
+
+  @media (min-width: 960px) {
+    padding-top: 100px; /* Header height for desktop */
+  }
 `;
 
 ////////////////////////////////////////////////////////////////////////////////
--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -1,20 +1,18 @@
 import React from "react";
 
-import StyledPageHeader,
-  { StyledPageHeaderAnchor, StyledPageHeaderH1, StyledPageHeaderH1Span } from "./StyledPageHeader";
-
-import "./index.css";
-import "./index-mobile.css";
+import StyledPageHeader, {
+    StyledPageHeaderAnchor,
+    StyledPageHeaderH1,
+    StyledPageHeaderH1Span
+} from "./StyledPageHeader";
 
 const PageHeader = () =>
   <StyledPageHeader>
-    <div>
-      <StyledPageHeaderH1>
-        <StyledPageHeaderAnchor href="#top">
-          Walmyr <StyledPageHeaderH1Span>Lima e Silva </StyledPageHeaderH1Span>Filho
-        </StyledPageHeaderAnchor>
-      </StyledPageHeaderH1>
-    </div>
+    <StyledPageHeaderH1>
+      <StyledPageHeaderAnchor href="#top">
+        Walmyr <StyledPageHeaderH1Span>Lima e Silva </StyledPageHeaderH1Span>Filho
+      </StyledPageHeaderAnchor>
+    </StyledPageHeaderH1>
   </StyledPageHeader>
 
 export default PageHeader;

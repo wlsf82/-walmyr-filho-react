1bb7ffd417b1ad57803345719752cf1acdb7fe0f
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2019-06-01 17:10:52 +0200

    Add background pattern in Introduction

--- src/components/Introduction/StyledIntroduction.js
+++ src/components/Introduction/StyledIntroduction.js
@@ -11,8 +11,8 @@ const StyledIntroduction = styled.div`
   font-weight: 100;
   font-size: 120%;
   line-height: 120%;
-  background-color: #ffffff;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1327' height='1327' viewBox='0 0 100 100'%3E%3Cg stroke='%23CCC' stroke-width='26.1' stroke-opacity='0.12'%3E%3Crect fill='%23F5F5F5' x='-60' y='-60' width='110' height='240'/%3E%3C/g%3E%3C/svg%3E");
+  background: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1IiBoZWlnaHQ9IjUiPgo8cmVjdCB3aWR0aD0iNSIgaGVpZ2h0PSI1IiBmaWxsPSIjZmZmIj48L3JlY3Q+CjxyZWN0IHdpZHRoPSIxIiBoZWlnaHQ9IjEiIGZpbGw9IiNjY2MiPjwvcmVjdD4KPC9zdmc+");
+}
 
 @media (min-width: 960px) {
   min-height: 50vh;

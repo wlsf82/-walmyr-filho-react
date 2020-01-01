f8420fb7c6d0ae272fa285f362aad5f14c97e681
Walmyr Filho <limaesilva@qelp.com>
2020-01-01 22:02:30 +0100

    Replace course with a new one
    For now I'll keep only the courses in portuguese, and as soon as
    I launch a new one in English (soon), I'll re-add the removed one.

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -19,18 +19,24 @@ const topCourses = [
   {
     id: uuid(),
     href: "http://talkingabouttesting.coursify.me/courses/testes-automatizados-com-cypress-basico",
-    title: "Cypress",
-    p: "Curso prático sobre testes automatizados com Cypress (básico)",
+    title: "Cypress básico",
+    p: "Curso prático sobre testes automatizados com Cypress utilizando funcionalidades básicas",
   },
+  {
+    id: uuid(),
+    href: "https://talkingabouttesting.coursify.me/courses/testes-automatizados-com-cypress-intermediario#",
+    title: "Cypress intermediário",
+    p: "Curso prático sobre como otimizar testes de GUI utilizando chamadas de APIs",
+  },
+];
+
+const bottomCourses = [
   {
     id: uuid(),
     href: "http://talkingabouttesting.coursify.me/courses/arquitetura-de-testes-com-protractor",
     title: "Protractor",
     p: "Curso prático sobre arquitetura de testes end-to-end com o framework Protractor",
   },
-];
-
-const bottomCourses = [
   {
     id: uuid(),
     href: "http://talkingabouttesting.coursify.me/courses/testes-de-regressao-visual-com-backstopjs",
@@ -42,12 +48,6 @@ const bottomCourses = [
     href: "http://talkingabouttesting.coursify.me/courses/mini-curso-testes-de-regressao-visual-com-backstop-config",
     title: "backstop-config",
     p: "Curso prático sobre testes de regressão visual para webistes estáticos",
-  },
-  {
-    id: uuid(),
-    href: "https://www.udemy.com/course/introduction-to-test-automation",
-    title: "Test automation",
-    p: "Theoretical course about test automation in many application layers",
   },
 ];
 

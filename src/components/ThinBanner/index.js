b1878d4af5e846323e34922ed95ec2b2c85e6255
Walmyr <wlsf82@gmail.com>
2020-06-17 17:08:44 +0200

    Remove unnecessary style from new component

--- src/components/ThinBanner/index.js
+++ src/components/ThinBanner/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 const ThinBanner = () => (
-  <div style={{
-      padding: '10px 10px 30px 10px',
-      letterSpacing: '0.8px'
-    }}>
+  <div style={{ padding: '10px 10px 30px 10px' }}>
     <span>Os curso da Escola TAT também estão disponíveis no </span>
     <a
       href="https://udemy.com/user/walmyr"

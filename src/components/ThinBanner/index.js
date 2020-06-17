c9f34399252d0b68d1f4d065b6db74259a94fe60
Walmyr <wlsf82@gmail.com>
2020-06-17 18:27:44 +0200

    Fix typo on thin banner component

--- src/components/ThinBanner/index.js
+++ src/components/ThinBanner/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ThinBanner = () => (
   <div style={{ padding: '10px 10px 30px 10px' }}>
-    <span>Os curso da Escola TAT também estão disponíveis no </span>
+    <span>Os cursos da Escola TAT também estão disponíveis no </span>
     <a
       href="https://udemy.com/user/walmyr"
       target="_blank"

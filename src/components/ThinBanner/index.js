e2624d9e4d82f87a2b027deb8f40df66e9a8594a
Walmyr <wlsf82@gmail.com>
2021-01-12 00:21:45 +0100

    Update component with missing link

--- src/components/ThinBanner/index.js
+++ src/components/ThinBanner/index.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const ThinBanner = () => (
   <div style={{ padding: '10px 10px 30px 10px' }}>
-    <span>Os cursos da Escola TAT também estão disponíveis no </span>
+    <span>Os cursos da </span>
+    <a
+      href="https://talkingabouttesting.coursify.me"
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ textDecoration: 'underline', fontWeight: 'bold' }}
+    >Escola TAT</a>
+    <span> também estão disponíveis no </span>
     <a
       href="https://udemy.com/user/walmyr"
       target="_blank"

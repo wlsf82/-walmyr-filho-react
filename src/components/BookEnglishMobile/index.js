33be41c2b7f1b9c4e07936df1f2122ae53239f68
Walmyr <wlsf82@gmail.com>
2022-12-04 19:21:33 +0100

    Shorten book english description mobile

--- src/components/BookEnglishMobile/index.js
+++ src/components/BookEnglishMobile/index.js
@@ -27,7 +27,7 @@ const Book = () =>
           <StyledBookH2>{ book }</StyledBookH2>
           <StyledBookH3>{ bookTitle }</StyledBookH3>
           <StyledBookH4>{ bookSubTitle }</StyledBookH4>
-          <p>In this book, you will find a collection of best practices to implement automated end-to-end tests in the development process of web applications, using the framework Protractor.</p>
+          <p>A collection of best practices to implement automated end-to-end tests in the development process of web applications, using the framework Protractor.</p>
           <BuyBookButton hrefValue={ bookUrl }>Buy the book</BuyBookButton>
         </StyleBookDescription>
         <a href={bookUrl} target={targetValue} rel={relValue}>
